6cd91ebd2a643d92fd1fd4c7a02c930e0d1d7636
diananov11 <ohlemon0678@gmail.com>
2024-10-03 23:58:01 +0700

    add error page and router dom

---
+++ src/pages/Error.jsx
@@ -0,0 +1,16 @@
+import { NavLink, useNavigate } from "react-router-dom"
+
+
+const Error = () => {
+  return (
+    <>
+      <div className="error min-vh-100 text-white text-center align-content-center" >
+        <h1> OOOPSS!! this file doesn't exist </h1>
+        <h3>404 - Page Not Found..</h3>
+        <NavLink to='/' className="text-white">Back to home</NavLink>
+      </div>
+    </>
+  )
+}
+
+export default Error
